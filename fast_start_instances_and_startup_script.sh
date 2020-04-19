01ccda36f98141c915815e246b9a6eec1c3a6ac9
ivan <ivanmmazur@gmail.com>
2020-04-19 17:57:56 +0200

    les_6

---
+++ fast_start_instances_and_startup_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gcloud compute instances create app-autofile \
+    --boot-disk-size=10GB \
+    --image-family ubuntu-1604-lts \
+    --image-project=ubuntu-os-cloud \
+    --machine-type=g1-small \
+    --tags puma-server \
+    --zone=us-central1-a \
+    --restart-on-failure \
+    --metadata-from-file startup-script=./startup_script.sh
