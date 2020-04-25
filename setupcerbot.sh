ee4ec941884a48e1fa41a019ebc0f89c992a5e96
ivan <ivanmmazur@gmail.com>
2020-04-25 21:19:53 +0200

    base HW#7

--- setupcerbot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install software-properties-common -y
-add-apt-repository universe -y
-add-apt-repository ppa:certbot/certbot -y
-apt-get update
-apt-get install certbot -y
