539e5c44a01fc8ac8632fe499d7af71ce32fe2c9
Ivan Mazur <ivanmmazur@gmail.com>
2020-06-13 20:17:59 +0200

    problem when creating an image (Timeout waiting for SSH)

---
+++ terraform/db.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "db" {
+  name         = "reddit-db"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags = ["reddit-db"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.db_disk_image}"
+    }
+  }
+  metadata {
+    ssh-keys = "ivanmazur:${file(var.public_key_path)}"
+  }
+    network_interface {
+    network = "default"
+    access_config = {}
+  }
+}
+
+resource "google_compute_firewall" "firewall_mongo" {
+  name = "allow-mongo-default"
+
+  # Name of the natwork in which the rule applies
+  network = "default"
+
+  # What access to allow
+  allow {
+    protocol = "tcp"
+    ports    = ["27017"]
+  }
+
+  # What addresses are allowed access
+  target_tags   = ["reddit-db"]
+  source_tags   = ["reddit-app"]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,85 +13,80 @@ provider "google" {
   region = "${var.region}"
 }
 
-resource "google_compute_project_metadata" "default" {
-  metadata {
-    ssh-keys = "appuser1:${file(var.public_key_path)} appuser2:${file(var.public_key_path)} ivanmazur:${file(var.public_key_path)}"
-  }
-}
+# resource "google_compute_project_metadata" "default" {
+#   metadata {
+#     ssh-keys = "appuser1:${file(var.public_key_path)} appuser2:${file(var.public_key_path)} ivanmazur:${file(var.public_key_path)}"
+#   }
+# }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "europe-west1-b"
-  tags = ["reddit-app"]
+# resource "google_compute_instance" "app" {
+#   name         = "reddit-app"
+#   machine_type = "g1-small"
+#   zone         = "europe-west1-b"
+#   tags = ["reddit-app"]
 
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "${var.disk_image}"
+#     }
+#   }
 
-  metadata {
-    ssh-keys = "ivanmazur:${file(var.public_key_path)}"
-  }
+#   metadata {
+#     ssh-keys = "ivanmazur:${file(var.public_key_path)}"
+#   }
 
-  # network_interface {
-  #   network       = "default"
-  #   access_config {}
-  # }
+#   connection {
+#     type        = "ssh"
+#     user        = "ivanmazur"
+#     agent       = false
+#     private_key = "${file(var.private_key_path)}"
+#   }
 
-  connection {
-    type        = "ssh"
-    user        = "ivanmazur"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
+#   network_interface {
+#     network = "default"
+#     access_config = {
+#       nat_ip = "${google_compute_address.app_ip.address}"
+#     }
+#   }
+#   provisioner "file" {
+#     source      = "files/puma.service"
+#     destination = "/tmp/puma.service"
+#   }
 
-  network_interface {
-    network = "default"
-    access_config = {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
+#   provisioner "remote-exec" {
+#     script = "files/deploy.sh"
+#   }
+# }
 
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
+# resource "google_compute_address" "app_ip" {
+#   name = "reddit-app-ip"
+# }
 
-resource "google_compute_address" "app_ip" {
-  name = "reddit-app-ip"
-}
+# resource "google_compute_firewall" "firewall_puma" {
+#   name = "allow-puma-default"
 
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+#   # Name of the natwork in which the rule applies
+#   network = "default"
 
-  # Name of the natwork in which the rule applies
-  network = "default"
+#   # What access to allow
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["9292"]
+#   }
 
-  # What access to allow
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
+#   # What addresses are allowed access
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["reddit-app"]
+# }
 
-  # What addresses are allowed access
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
-}
+# resource "google_compute_firewall" "firewall_ssh" {
+#   name = "default-allow-ssh"
+#   network = "default"
 
-resource "google_compute_firewall" "firewall_ssh" {
-  name = "default-allow-ssh"
-  network = "default"
+#   allow {
+#     protocol = "tcp"
+#     ports = ["22"]
+#   }
 
-  allow {
-    protocol = "tcp"
-    ports = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
+#   source_ranges = ["0.0.0.0/0"]
+# }
